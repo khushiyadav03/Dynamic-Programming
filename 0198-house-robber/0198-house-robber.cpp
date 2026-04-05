7789d811ddc7e855ad6c3cc919933c07e811ddd3
Khushi Yadav <ky0307005@gmail.com>
2026-04-05 15:35:25 +0530

    Time: 0 ms (100%), Space: 10.5 MB (60.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,12 +3,10 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+2, 0);
+        dp[n] = 0, dp[n+1] = 0;
 
         for(int i = n-1; i>=0; i--){
-            int pick = nums[i] + dp[i+2];
-            int skip = dp[i+1];
-
-            dp[i] = max(pick, skip);
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1]);
         }
         return dp[0];
     }
