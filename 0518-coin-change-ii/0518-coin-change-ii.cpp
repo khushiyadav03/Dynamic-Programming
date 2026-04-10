352bbcd582034f65b99cc01656d0c327e85ff0ee
Khushi Yadav <ky0307005@gmail.com>
2026-04-10 23:46:52 +0530

    Time: 47 ms (23.51%), Space: 36.8 MB (39.58%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int f(int i, int amount, vector<int>& coins, vector<vector<int>>& dp){
+        if(amount == 0) return 1;
+        if(i == coins.size()) return 0;
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int skip = f(i+1, amount, coins, dp);
+
+        int take = 0;
+        if(amount >= coins[i]){
+            take = f(i, amount-coins[i], coins, dp);
+        }
+
+        return dp[i][amount] = take + skip;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return f(0, amount, coins, dp);
+    }
+};
